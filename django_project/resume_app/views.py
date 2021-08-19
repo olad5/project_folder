ed770086eecb0c5e9d9c2d9aa924e9c9fe8bd3b7
shonubioladipo@gmail.com <shonubioladipo@gmail.com>
2021-08-19 14:01:49 +0100

    basic form structure shows up in the view

--- django_project/resume_app/views.py
+++ django_project/resume_app/views.py
@@ -8,4 +8,17 @@ from .forms import ContactForm
 
 
 def index(request):
-    return render(request, 'resume_app/index.html')
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact_name = form.cleaned_data['contact_name']
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            print(f"Contact Name is {contact_name}")
+            print(f"Subject is {subject}")
+            print(f"From {from_email}")
+            print(f"The message is {message}")
+    return render(request, 'resume_app/index.html', {'form': form})
